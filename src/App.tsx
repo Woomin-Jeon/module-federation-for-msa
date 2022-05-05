a8cb84d159bf216b020d08809d29d6bff105de3f
Woomin-Jeon <dal96k@gmail.com>
2022-05-05 12:40:32 +0900

    init: project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <h1>My App!</h1>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = () => ({
+  entry: {
+    index: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: require.resolve('babel-loader'),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+    },
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: {
+      index: './src/index.html',
+    },
+  },
+});
